7b8c377285f7bbc2c654e998b02f24a513fb44a9
vladlen11 <vladlen11.89@rambler.ru>
2017-04-01 11:13:04 +0300

    new branch

---
+++ js/common.js
@@ -0,0 +1,118 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+    /**
+     * =======================================
+     * Contact Form Style
+     * =======================================
+     */
+    $( '.form-control' ).each( function( inputEl ) {
+
+      // in case the input is already filled..
+      if( $(this).val() !== '' ) {
+        $(this).closest('.input-outer').addClass('input-filled');
+      }
+
+      // events:
+      $( this ).focus(function(){
+
+        $(this).closest('.input-outer').addClass('input-filled');
+
+      });
+
+      $( this ).blur(function(){
+
+          if( $(this).val() === '' ) {
+            $(this).closest('.input-outer').removeClass('input-filled');
+          }
+
+      });
+
+    });
+    /**
+     * ============================
+     * CONTACT FORM 2
+     * ============================
+    */
+    $("#contact-form").on('submit', function(e) {
+      e.preventDefault();
+      var success = $(this).find('.email-success'),
+        failed = $(this).find('.email-failed'),
+        loader = $(this).find('.email-loading'),
+        postUrl = $(this).attr('action');
+
+      var data = {
+        name: $(this).find('.contact-name').val(),
+        email: $(this).find('.contact-email').val(),
+        company: $(this).find('.contact-company').val(),
+        subject: $(this).find('.contact-subject').val(),
+        message: $(this).find('.contact-message').val()
+      };
+
+      if ( isValidEmail(data['email']) && (data['message'].length > 1) && (data['name'].length > 1) ) {
+        $.ajax({
+          type: "POST",
+          url: postUrl,
+          data: data,
+          beforeSend: function() {
+            loader.fadeIn(1000);
+          },
+          success: function(data) {
+            loader.fadeOut(1000);
+            success.delay(500).fadeIn(1000);
+            failed.fadeOut(500);
+          },
+          error: function(xhr) { // if error occured
+            loader.fadeOut(1000);
+            failed.delay(500).fadeIn(1000);
+            success.fadeOut(500);
+          },
+          complete: function() {
+            loader.fadeOut(1000);
+          }
+        });
+      } else {
+        loader.fadeOut(1000);
+        failed.delay(500).fadeIn(1000);
+        success.fadeOut(500);
+      }
+
+      return false;
+    });
+});
